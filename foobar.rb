8c83062679af7c767d242bdefb2ccafc87cc2246
Roberto Romo <rromo@berkeley.edu>
2016-09-20 10:03:26 -0700

    finished hw1

--- foobar.rb
+++ foobar.rb
@@ -2,6 +2,16 @@ class Foobar
 
   def self.baz(a)
     # Class method
+    #changes each string into an integer, adds two to each number, 
+    #keeps the even numbers only, removes any duplicates, rejects the 
+    #resulting numbers greater than ten, and finally returns the sum of the resultingarray.
+
+    a = a.map(&:to_i)
+    a = a.map{|x| x += 2}
+    a.delete_if{|x| x%2 != 0 }
+   	a.uniq
+   	a.delete_if{|x| x > 10}
+   	return a.inject(0, :+)
     # Call with `Foobar.baz`
   end
 end
--- hw1.rb
+++ hw1.rb
@@ -1,15 +1,19 @@
 def squared_sum(a, b)
   # Q1 CODE HERE
+  return (a+b)**2
 
 end
 
 def sort_array_plus_one(a)
   # Q2 CODE HERE
+  # Destructive
+   return a.sort.map{|int| int += 1}
 
 end
 
 def combine_name(first_name, last_name)
   # Q3 CODE HERE
+  return [first_name, last_name].join(' ')
 
 end
 
